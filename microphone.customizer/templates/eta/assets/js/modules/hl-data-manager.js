d970530e5c5e277bcca2f7b025e9782249906a7f
Abstract Mindfreak <mungorodnaneve@gmail.com>
2026-03-18 10:53:31 +0300

    Refine eta HL data mapping helpers

--- microphone.customizer/templates/eta/assets/js/modules/hl-data-manager.js
+++ microphone.customizer/templates/eta/assets/js/modules/hl-data-manager.js
@@ -7,20 +7,32 @@ import { debugLog } from '../utils/debug.js';
 
 const MALFA_VARIANTS = new Set(['malfasilver', 'malfagold']);
 
+function parseBoolFlag(value) {
+    return value === 1 || value === true;
+}
+
+function parseModelId(value) {
+    return parseInt(value || '0', 10) || 0;
+}
+
+function getCurrentModelData(modelCode) {
+    return window.CUSTOMIZER_DATA?.modelsByCode?.[modelCode] || null;
+}
+
 function mapOptionToState(option) {
-    const isRal = option.UF_IS_RAL === 1 || option.UF_IS_RAL === true;
+    const isRal = parseBoolFlag(option.UF_IS_RAL);
     const ralCode = option.RAL_DATA?.UF_CODE || null;
     return {
         variantCode: option.UF_VARIANT_CODE || '',
         variant: option.UF_VARIANT_CODE || '',
         variantName: option.UF_VARIANT_NAME || '',
         isRal,
-        isFree: option.UF_IS_FREE === 1 || option.UF_IS_FREE === true,
+        isFree: parseBoolFlag(option.UF_IS_FREE),
         color: ralCode,
         colorValue: option.RAL_DATA?.UF_HEX || null,
         colorName: option.RAL_DATA?.UF_NAME || null,
         floodOpacity: option.RAL_DATA?.UF_FLOOD_OPACITY || null,
-        modelId: option.UF_MODEL_ID || 0,
+        modelId: parseModelId(option.UF_MODEL_ID),
         svgTargetMode: option.UF_SVG_TARGET_MODE || null,
         svgLayerGroup: option.UF_SVG_LAYER_GROUP || null,
         svgFilterId: option.UF_SVG_FILTER_ID || null,
@@ -29,18 +41,20 @@ function mapOptionToState(option) {
 }
 
 function mapOptionForSection(option) {
-    const isRal = option.UF_IS_RAL === 1 || option.UF_IS_RAL === true;
+    const isRal = parseBoolFlag(option.UF_IS_RAL);
+    const isFree = parseBoolFlag(option.UF_IS_FREE);
+    const isRalPaid = parseBoolFlag(option.IS_RAL_PAID) ? true : !isFree;
     return {
         variantCode: option.UF_VARIANT_CODE || '',
         variantName: option.UF_VARIANT_NAME || '',
         isRal,
-        isFree: option.UF_IS_FREE === 1 || option.UF_IS_FREE === true,
-        isRalPaid: option.IS_RAL_PAID === 1 || option.IS_RAL_PAID === true ? true : !(option.UF_IS_FREE === 1 || option.UF_IS_FREE === true),
+        isFree,
+        isRalPaid,
         price: parseInt(option.UF_PRICE || '0', 10) || 0,
         color: option.UF_RAL_COLOR_CODE || '',
         colorValue: option.RAL_DATA?.UF_HEX || '',
         colorName: option.RAL_DATA?.UF_NAME || '',
-        modelId: parseInt(option.UF_MODEL_ID || '0', 10) || 0,
+        modelId: parseModelId(option.UF_MODEL_ID),
         svgTargetMode: option.UF_SVG_TARGET_MODE || '',
         svgLayerGroup: option.UF_SVG_LAYER_GROUP || '',
         svgFilterId: option.UF_SVG_FILTER_ID || '',
@@ -90,12 +104,12 @@ function syncInitialLogoBackgroundState() {
 
 export function buildCurrentModelOptions(modelCode) {
     const data = window.CUSTOMIZER_DATA || {};
-    const model = data.modelsByCode?.[modelCode];
+    const model = getCurrentModelData(modelCode);
     if (!model) {
         return {};
     }
 
-    const currentModelId = parseInt(model.ID || '0', 10) || 0;
+    const currentModelId = parseModelId(model.ID);
     const currentSeries = String(model.MODEL_SERIES || '');
     const allowMalfaLogos = supportsMalfaLogos(modelCode);
     const allOptions = data.options || {};
@@ -120,7 +134,7 @@ export function buildCurrentModelOptions(modelCode) {
 
     Object.keys(merged).forEach((sectionCode) => {
         merged[sectionCode] = merged[sectionCode].filter((option) => {
-            const optionModelId = parseInt(option.UF_MODEL_ID || '0', 10) || 0;
+            const optionModelId = parseModelId(option.UF_MODEL_ID);
             if (optionModelId > 0 && optionModelId !== currentModelId) {
                 return false;
             }
@@ -143,7 +157,7 @@ export function buildCurrentModelOptions(modelCode) {
 }
 
 export function syncCurrentModelOptionData(modelCode) {
-    const model = window.CUSTOMIZER_DATA?.modelsByCode?.[modelCode] || {};
+    const model = getCurrentModelData(modelCode) || {};
     const currentModelOptions = buildCurrentModelOptions(modelCode);
     const sectionOptions = Object.fromEntries(
         Object.entries(currentModelOptions).map(([sectionKey, options]) => [
@@ -153,7 +167,7 @@ export function syncCurrentModelOptionData(modelCode) {
     );
 
     window.CUSTOMIZER_DATA.currentModelCode = modelCode;
-    window.CUSTOMIZER_DATA.currentModelId = parseInt(model.ID || '0', 10) || 0;
+    window.CUSTOMIZER_DATA.currentModelId = parseModelId(model.ID);
     window.CUSTOMIZER_DATA.currentModelOptions = currentModelOptions;
     window.CUSTOMIZER_DATA.sectionOptions = sectionOptions;
     window.CUSTOMIZER_DATA.optionsBySection = sectionOptions;
@@ -181,7 +195,7 @@ export function initHLDataManager() {
 
     stateManager.set('hlData', buildInitialHlData(data));
 
-    const currentModel = data.modelsByCode?.[data.currentModelCode] || null;
+    const currentModel = getCurrentModelData(data.currentModelCode);
     debugLog('[HL Data Manager] Current model:', currentModel);
     debugLog('[HL Data Manager] MODEL_SERIES:', currentModel?.MODEL_SERIES);
     debugLog('[HL Data Manager] DEFAULT_SHOCKMOUNT_OPTION:', currentModel?.UF_DEFAULT_SHOCKMOUNT_OPTION);
